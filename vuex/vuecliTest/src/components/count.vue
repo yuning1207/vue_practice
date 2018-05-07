d24fd5197220ed567a297d9b97b7ed821c692ea6
yuning1207 <1578517699@qq.com>
2018-05-07 22:40:56 +0800

    vuex学习

---
+++ vuex/vuecliTest/src/components/count.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h2>{{msg}}</h2>
+        <hr/>
+        <h3>{{$store.state.count}}--{{count}}</h3>
+        <button @click="$store.commit('add',10)">+</button> 
+        <button @click="reduce">-</button>
+    </div>
+</template>
+<script>
+    import store from '@/vuex/store';
+    import {mapState,mapMutations} from 'vuex';
+    export default{
+        data(){
+            return{
+                msg:'Hello Vuex'
+            }
+        },
+        store,
+        // computed:{
+        //     count(){
+        //         return this.$store.state.count;
+        //     }
+        // }
+        computed:
+            // mapState({
+            //     count:state=>state.count
+            // })
+            mapState(['count']),
+            methods:mapMutations(['add','reduce'])
+    }
+</script>
--- vuex/vuecliTest/src/router/index.js
+++ vuex/vuecliTest/src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import count from '@/components/count'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [{
+            path: '/',
+            name: 'HelloWorld',
+            component: HelloWorld
+        },
+        {
+            path: '/count',
+            component: count
+        }
+    ]
 })
