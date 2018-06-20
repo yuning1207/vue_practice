818fd437e80ae28847cb439583ad32ad926d58fc
yuning1207 <1578517699@qq.com>
2018-06-20 22:55:37 +0800

    不落阁vue配置

---
+++ 不落阁/blog/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <header></header>
+    <div class="main"><router-view/></div>
+  </div>
+</template>
+
+<script>
+import header from '@/components/common/header'
+export default {
+  name: 'App',
+  components:{
+    header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
